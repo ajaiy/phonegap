a27e8ace7a312105628a0ebbc956ba50158e5f01
Ajaiy J <ajaiy@tqube.com>
2013-03-28 07:22:16 +0530

    initial commit

---
+++ gen/R.java.d
@@ -0,0 +1,14 @@
+D:\phoneGap\lib\android\bin\example\gen\org\apache\cordova\example\R.java \
+ : D:\phoneGap\lib\android\bin\example\res\drawable\icon.png \
+D:\phoneGap\lib\android\bin\example\res\drawable-hdpi\icon.png \
+D:\phoneGap\lib\android\bin\example\res\drawable-hdpi\ic_launcher.png \
+D:\phoneGap\lib\android\bin\example\res\drawable-ldpi\icon.png \
+D:\phoneGap\lib\android\bin\example\res\drawable-ldpi\ic_launcher.png \
+D:\phoneGap\lib\android\bin\example\res\drawable-mdpi\icon.png \
+D:\phoneGap\lib\android\bin\example\res\drawable-mdpi\ic_launcher.png \
+D:\phoneGap\lib\android\bin\example\res\drawable-xhdpi\icon.png \
+D:\phoneGap\lib\android\bin\example\res\drawable-xhdpi\ic_launcher.png \
+D:\phoneGap\lib\android\bin\example\res\layout\main.xml \
+D:\phoneGap\lib\android\bin\example\res\values\strings.xml \
+D:\phoneGap\lib\android\bin\example\res\xml\config.xml \
+D:\phoneGap\lib\android\bin\example\bin\AndroidManifest.xml \
